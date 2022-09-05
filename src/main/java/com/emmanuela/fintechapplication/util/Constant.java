542cced9b77e8409e910ff6b1521488c16ad8dd1
DevEmmanuela <ikeemmanuelachinwe@gmail.com>
2022-09-05 20:18:19 +0100

    feat/docker and docker compose

--- src/main/java/com/emmanuela/fintechapplication/util/Constant.java
+++ src/main/java/com/emmanuela/fintechapplication/util/Constant.java
@@ -5,7 +5,7 @@ public class Constant {
     public static final String CREATE_VIRTUAL_ACCOUNT_API = "https://api.flutterwave.com/v3/virtual-account-numbers";
     public static final String AUTHORIZATION = "FLWSECK_TEST-2f4e23ee51c1ba35a80337a3ae24b60a-X";
 
-    public static final String EMAIL_VERIFICATION_LINK = "http://localhost:3333/api/v1/confirmToken?token=";
+    public static final String EMAIL_VERIFICATION_LINK = "http://localhost:3333/confirmToken?token=";
     public static final String GET_BANKS_API = "https://api.flutterwave.com/v3/banks/";
     public static final String RESOLVE_ACCOUNT_API = "https://api.flutterwave.com/v3/accounts/resolve";
     public static final String OTHER_BANK_TRANSFER = "https://api.flutterwave.com/v3/transfers";
@@ -13,7 +13,7 @@ public class Constant {
 
     public static final String KEYS="secret";
 
-    public static final String VERIFY_TRANSFER = "http://localhost:8085/api/v1/transfer/verify-transfer";
+    public static final String VERIFY_TRANSFER = "http://localhost:3333/transfer/verify-transfer";
     public static final String VERIFY_TRANSACTION = "https://api.flutterwave.com/v3/transactions/:";
 
 }
