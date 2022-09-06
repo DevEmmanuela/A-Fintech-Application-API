860ef10427812a71400d68825e4c339b233f9316
DevEmmanuela <ikeemmanuelachinwe@gmail.com>
2022-09-06 08:54:18 +0100

    feat/test key

--- src/main/java/com/emmanuela/fintechapplication/util/Constant.java
+++ src/main/java/com/emmanuela/fintechapplication/util/Constant.java
@@ -3,16 +3,16 @@ package com.emmanuela.fintechapplication.util;
 public class Constant {
 
     public static final String CREATE_VIRTUAL_ACCOUNT_API = "https://api.flutterwave.com/v3/virtual-account-numbers";
-    public static final String AUTHORIZATION = "FLWSECK-662e890e70669994954401be4d9f73c1-X";
+    public static final String AUTHORIZATION = "FLWSECK_TEST-2f4e23ee51c1ba35a80337a3ae24b60a-X";
 
-    public static final String EMAIL_VERIFICATION_LINK = "http://localhost:3333/confirmToken?token=";
+    public static final String EMAIL_VERIFICATION_LINK = "https://fintech-application.herokuapp.com//confirmToken?token=";
     public static final String GET_BANKS_API = "https://api.flutterwave.com/v3/banks/";
     public static final String RESOLVE_ACCOUNT_API = "https://api.flutterwave.com/v3/accounts/resolve";
     public static final String OTHER_BANK_TRANSFER = "https://api.flutterwave.com/v3/transfers";
     public static final String STATUS = "PENDING";
     public static final String KEYS="secret";
 
-    public static final String VERIFY_TRANSFER = "http://localhost:3333/transfer/verify-transfer";
+    public static final String VERIFY_TRANSFER = "https://fintech-application.herokuapp.com//transfer/verify-transfer";
     public static final String VERIFY_TRANSACTION = "https://api.flutterwave.com/v3/transactions/:";
 
 }
