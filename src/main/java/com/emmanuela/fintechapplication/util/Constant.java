7794de11b457c141fe32a8e443001b79f7e0eeb5
DevEmmanuela <ikeemmanuelachinwe@gmail.com>
2022-09-11 15:35:21 +0100

    feat/authorization

--- src/main/java/com/emmanuela/fintechapplication/util/Constant.java
+++ src/main/java/com/emmanuela/fintechapplication/util/Constant.java
@@ -1,10 +1,12 @@
 package com.emmanuela.fintechapplication.util;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class Constant {
 
     public static final String CREATE_VIRTUAL_ACCOUNT_API = "https://api.flutterwave.com/v3/virtual-account-numbers";
-    public static final String AUTHORIZATION = "FLWSECK-662e890e70669994954401be4d9f73c1-X";
-
+    @Value("${FLW_SECRET_KEY}")
+    public static String AUTHORIZATION;
     public static final String EMAIL_VERIFICATION_LINK = "https://fintech-application.herokuapp.com/confirmToken?token=";
     public static final String GET_BANKS_API = "https://api.flutterwave.com/v3/banks/";
     public static final String RESOLVE_ACCOUNT_API = "https://api.flutterwave.com/v3/accounts/resolve";
